affbaa02b24b802519179530ee1701171b3e2e14
jbt <jbt@307_ins>
2019-11-24 14:56:12 +0200

    aaa

---
+++ Lesson 01/src/d/arrays/Demo1.java
@@ -0,0 +1,26 @@
+package d.arrays;
+
+import java.util.Arrays;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// System.out.println(args.length);
+		// create array of int
+		int[] arr = new int[5];
+		// populate with elements
+		arr[0] = 7;
+		arr[1] = 3;
+		arr[2] = 9;
+
+		// access array elements
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+		// print entire array as string
+		System.out.println(Arrays.toString(arr));
+	}
+}
